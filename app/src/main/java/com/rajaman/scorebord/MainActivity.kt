d83618018683a1b092a9547f6fd8a4ca2f9b2445
LordSam07 <49715934+LordSam07@users.noreply.github.com>
2020-03-01 14:48:13 +0530

    new

--- app/src/main/java/com/rajaman/scorebord/MainActivity.kt
+++ app/src/main/java/com/rajaman/scorebord/MainActivity.kt
@@ -2,11 +2,52 @@ package com.rajaman.scorebord
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var teamA = textViewA.text.toString().toInt()
+        var teamB = textViewA.text.toString().toInt()
+
+        buttonA1.setOnClickListener {
+            teamA += 1
+            textViewA.text = teamA.toString()
+        }
+
+        buttonA2.setOnClickListener {
+            teamA += 2
+            textViewA.text = teamA.toString()
+        }
+
+        buttonA3.setOnClickListener {
+            teamA += 3
+            textViewA.text = teamA.toString()
+        }
+
+        buttonB1.setOnClickListener {
+            teamB += 1
+            textViewB.text = teamB.toString()
+        }
+
+        buttonB2.setOnClickListener {
+            teamB += 2
+            textViewB.text = teamB.toString()
+        }
+
+        buttonB3.setOnClickListener {
+            teamB += 3
+            textViewB.text = teamB.toString()
+        }
+
+        buttonReset.setOnClickListener {
+            teamA = 0
+            teamB = 0
+            textViewA.text = teamA.toString()
+            textViewB.text = teamB.toString()
+        }
     }
 }
